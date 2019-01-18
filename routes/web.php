3f77c9086a4ed1e4f563bd92658219ff41a648b6
vagrant <vagrant@homestead>
2019-01-18 08:46:49 +0000

    注册与登陆

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,8 @@
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
-Route::get('/','PagesController@root')->name('root');
+Route::get('/', 'PagesController@root')->name('root');
+
+Auth::routes();

226fccf2fc1d84aed2ad9d8fef4ed73ea840a64e
JimAdela <1608808586@qq.com>
2019-01-19 08:58:26 +0000

    收货地址列表

--- app/Models/User.php
+++ app/Models/User.php
@@ -29,4 +29,9 @@ class User extends Authenticatable {
 	protected $hidden = [
 		'password', 'remember_token',
 	];
+
+	public function addresses() {
+		return $this->hasMany(UserAddress::class);
+	}
+
 }

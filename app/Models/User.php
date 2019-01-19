8809cbd5598541ac6aa38554126544574ddb7c31
JimAdela <1608808586@qq.com>
2019-01-19 02:51:46 +0000

    邮箱验证中间件

---
+++ app/Models/User.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class User extends Authenticatable {
+	use Notifiable;
+
+	/**
+	 * The attributes that are mass assignable.
+	 *
+	 * @var array
+	 */
+	protected $fillable = [
+		'name', 'email', 'password', 'email_verified',
+	];
+
+	protected $casts = [
+		'email_verified' => 'boolean',
+	];
+
+	/**
+	 * The attributes that should be hidden for arrays.
+	 *
+	 * @var array
+	 */
+	protected $hidden = [
+		'password', 'remember_token',
+	];
+}
---
+++ resources/views/pages/email_verify_notice.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+@section('title', '提示')
+@section('content')
+    <div class="panel panel-default">
+        <div class="panel-heading">提示</div>
+        <div class="panel-body text-center">
+            <h1>请先验证邮箱</h1>
+            <a href="{{route('root')}}" class="btn btn-primary">返回首页</a>
+        </div>
+    </div>
+@endsection

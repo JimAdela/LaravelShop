8d00e138cf5053470a043d7c4346d57a4a73be14
JimAdela <1608808586@qq.com>
2019-01-24 00:41:11 +0000

    商品模型

---
+++ app/Models/Product.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model {
+	protected $fillable = [
+		'title', 'description', 'image', 'on_sale',
+		'rating', 'sold_count', 'review_count', 'price',
+	];
+
+	protected $casts = [
+		'on_sale' => 'boolean',
+	];
+
+	public function skus() {
+		return $this->hasMany(ProductSku::class);
+	}
+}
---
+++ database/migrations/2019_01_24_002738_create_product_skus_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductSkusTable extends Migration {
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up() {
+		Schema::create('product_skus', function (Blueprint $table) {
+			$table->increments('id');
+			$table->string('title');
+			$table->string('description');
+			$table->decimal('price', 10, 2);
+			$table->unsignedInteger('stock');
+			$table->unsignedInteger('product_id');
+			$table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down() {
+		Schema::dropIfExists('product_skus');
+	}
+}

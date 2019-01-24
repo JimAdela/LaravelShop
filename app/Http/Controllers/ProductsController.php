338441694ed2ad9d74f35429785d7dda806bc862
JimAdela <1608808586@qq.com>
2019-01-24 08:31:54 +0000

    用户界面- 商品筛选,排序

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -7,7 +7,34 @@ use Illuminate\Http\Request;
 
 class ProductsController extends Controller {
 	public function index(Request $request) {
-		$products = Product::query()->where('on_sale', true)->paginate(16);
-		return view('products.index', ['products' => $products]);
+        $builder = Product::query()->where('on_sale', true);
+        if ($search = $request->input('search', '')) {
+            $like = '%'.$search.'%';
+            $builder->where(function ($query) use ($like) {
+                $query->where('title', 'like', $like)
+                    ->orWhere('description', 'like', $like)
+                    ->orWhereHas('skus', function($query) use ($like) {
+                        $query->where('title', 'like', $like)
+                            ->orWhere('description', 'like', $like);
+                    });
+            });
+        }
+
+        if ($order = $request->input('order','')) {
+            if (preg_match('/^(.+)_(asc|desc)$/',$order, $m)) {
+                if (in_array($m[1], ['price','sold_count', 'rating'])) {
+                    $builder->orderBy($m[1],$m[2]);
+                }
+            }
+        }
+
+		$products = $builder->paginate(16);
+		return view('products.index', [
+            'products' => $products,
+            'filters'  => [
+                'search' => $search,
+                'order'  => $order,
+            ],
+        ]);
 	}
 }

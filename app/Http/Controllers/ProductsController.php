4d563c6f42d15904a359058c220e4078d08dfbd8
JimAdela <1608808586@qq.com>
2019-01-26 03:04:17 +0000

    用户界面 - 收藏商品列表

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -72,4 +72,14 @@ class ProductsController extends Controller {
         $user->favoriteProducts()->detach($product);
         return [];
     }
+	
+
+    public function favorites(Request $request)
+    {
+	    $products = $request->user()->favoriteProducts()->paginate(16);
+
+	    return view('products.favorites', ['products'=> $products]);
+    }
+
 }
+
--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,7 @@ Route::group(['middleware' => 'auth'], function () {
 	Route::get('/email_verify_notice', 'PagesController@emailVerifyNotice')->name('email_verify_notice');
 	Route::get('/email_verification/verify', 'EmailVerificationController@verify')->name('email_verification.verify');
 	Route::get('email_verification/send', 'EmailVerificationController@send')->name('email_verification.send');
+	Route::get('products/favorites', 'ProductsController@favorites')->name('products.favorites');
 
 	Route::group(['middleware' => 'email_verified'], function () {
 		Route::get('user_addresses', 'UserAddressesController@index')->name('user_addresses.index');
@@ -32,6 +33,7 @@ Route::group(['middleware' => 'auth'], function () {
 
 		Route::post('products/{product}/favorite','ProductsController@favor')->name('products.favor');
 		Route::delete('products/{product}/favorite','ProductsController@disfavor')->name('products.disfavor');
+		Route::get('products/favorites','ProductsController@favorites')->name('products.favorites');
 	});
 });
 
